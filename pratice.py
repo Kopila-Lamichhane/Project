919357518df0c87086b506a739720e8e4968d048
Kopila-Lamichhane <158706547+Kopila-Lamichhane@users.noreply.github.com>
2024-02-03 12:54:58 +0545

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,23 @@
+# print("Hello, World!")
+# a = int(input("enter the value of a:"))
+# b = int(input("enter the value of b:"))
+# total = a+b
+# print(f"sum of two numbers {a+b}")
+# print(a+b)
+#list
+
+# mark = [45, 68,90,23,97,34,80,56]
+# mark.remove(90)
+# print(mark)
+ #tuple
+#Dic
+myDic = {
+    "name": "kopila",
+    "age": 20,
+    "number": 9823456789
+
+}
+a = myDic["age"]
+print(a)
+
+
